6950fec6dcead16aa7e9b693b9af89007408e77b
Trueguy64 <99567745+Trueguy64@users.noreply.github.com>
2023-11-27 12:35:37 +0000

    Fixed next row column image missing

--- image-grider.py
+++ image-grider.py
@@ -1,18 +1,18 @@
 import cv2;
 import os
 
-imageInput = input('What is Your File Name (With extension)?: ')
-rowInput = int(input('Numbers of rows: '))
-columnInput = int(input('Numbers of columns: '))
-fileName = input('Name of Output File: ')
-fileExt = input('File Extension Output With Dot(e.g. .PNG, .JPG): ')
-path = input('Where do you want to save it?: ')
+#imageInput = input('What is Your File Name (With extension)?: ')
+#rowInput = int(input('Numbers of rows: '))
+#columnInput = int(input('Numbers of columns: '))
+#fileName = input('Name of Output File: ')
+#fileExt = input('File Extension Output With Dot(e.g. .PNG, .JPG): ')
+#path = input('Where do you want to save it?: ')
 
 image = cv2.imread(str(imageInput), cv2.IMREAD_UNCHANGED)
 dimensions = image.shape
 height = dimensions[0]/columnInput
 width = dimensions[1]/rowInput
-ranger = (rowInput*(columnInput-1)+1)
+ranger = (rowInput*columnInput)
 h = 0
 w = 0
 dw = width
@@ -22,14 +22,16 @@ for i in range(ranger):
         croppedImage = image[int(h):int(height),int(w):int(width)]
         cv2.imwrite(os.path.join(str(path),str(fileName)+str(i)+str(fileExt)),croppedImage)
     else:
-        w+=dw
-        width+=dw
-        croppedImage = image[int(h):int(height),int(w):int(width)]
-        cv2.imwrite(os.path.join(path,str(fileName)+str(i)+str(fileExt),croppedImage)
-        print(str(i)+", "+str(w)+":"+str(width)+", "+str(h)+":"+str(height))
         if width == dimensions[1]:
-            width = dimensions[1]/rowInput
-            w = 0
             h+=dh
             height+=dh
-
+            width = dimensions[1]/rowInput
+            w = 0
+            croppedImage = image[int(h):int(height),int(w):int(width)]
+            cv2.imwrite(os.path.join(path,str(fileName)+str(i)+str(fileExt)),croppedImage)
+        else:
+            w+=dw
+            width+=dw
+            croppedImage = image[int(h):int(height),int(w):int(width)]
+            cv2.imwrite(os.path.join(path,str(fileName)+str(i)+str(fileExt)),croppedImage)
+            print(str(i)+", "+str(w)+":"+str(width)+", "+str(h)+":"+str(height))
